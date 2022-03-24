93a47dc6b8a5ad0060c7a3d17e2bb5d09adc460c
mthomas7503 <97054832+mthomas7503@users.noreply.github.com>
2022-03-24 17:08:53 -0400

    commit tree 64e6a7297f805358a11751ccfb6a53025f0fd332

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Add your code here
+function submitData(name, email) {return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {'Content-type': 'application/json',
+            Accept: 'application/json'},
+    body: JSON.stringify({'name': `${name}`, 'email': `${email}`})
+    }).then(Response => Response.json()).then(user => console.log(user.id)).catch(function(error) {return alert(error.message)})}
+
